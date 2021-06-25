9f1673d0b713db22b9f4de82c2bec9a1536fc703
Aman Dwivedi <aman.dwivedi5@gmail.com>
2021-06-25 19:07:17 +0530

    fix(test): update test to match snapshot
    Signed-off-by: Aman Dwivedi <aman.dwivedi5@gmail.com>

--- src/App.test.js
+++ src/App.test.js
@@ -17,11 +17,10 @@
 */
 
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import App from "./App";
 
 test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const { asFragment } = render(<App />);
+  expect(asFragment()).toMatchSnapshot();
 });
