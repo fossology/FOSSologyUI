c6f603f88a5ad0520502a943cdd20dd468986ecf
Shruti3004 <mail2shruti.ag@gmail.com>
2021-07-12 14:33:41 +0530

    fix(copyright): Fix the copyright json to copyright array
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

---
+++ scripts/copyrightsScript.js
@@ -0,0 +1,40 @@
+/*
+ Copyright (C) 2021 Shruti Agarwal (mail2shruti.ag@gmail.com), Gaurav Mishra (mishra.gaurav@siemens.com)
+
+ SPDX-License-Identifier: GPL-2.0
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ version 2 as published by the Free Software Foundation.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+var csv = require("csv-parser");
+var fs = require("fs");
+
+var copyrights = [];
+
+fs.createReadStream("scripts/copyrights.csv")
+  .pipe(csv())
+  .on("data", function (data) {
+    copyrights.push(data.copyright);
+  })
+  .on("end", function () {
+    let uniqueCopyrights = [...new Set(copyrights)].sort();
+    fs.writeFile(
+      "scripts/copyrights.js",
+      JSON.stringify(uniqueCopyrights),
+      (err) => {
+        if (err) {
+          console.log("Unable to parse the CSV to generate copyrights array");
+        }
+      }
+    );
+  });
