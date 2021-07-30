dbb0a3155c16151da3029b050104afd877808198
Aman Dwivedi <aman.dwivedi5@gmail.com>
2021-07-30 17:22:15 +0530

    fix(reuse): update handle change function
    Signed-off-by: Aman Dwivedi <aman.dwivedi5@gmail.com>

--- src/pages/Jobs/ScheduleAgents/index.jsx
+++ src/pages/Jobs/ScheduleAgents/index.jsx
@@ -157,7 +157,10 @@ const ScheduleAgents = () => {
         ...scanFileData,
         reuse: {
           ...scanFileData.reuse,
-          [e.target.name]: e.target.checked,
+          [e.target.name]:
+            e.target.type === "checkbox"
+              ? e.target.checked
+              : parseInt(e.target.value, 10) || e.target.value,
         },
       });
     }
