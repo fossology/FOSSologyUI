dffc9454271b0f254ef99c59f86476349e5d344b
Gaurav Mishra <gmishx@gmail.com>
2021-07-30 11:38:22 +0530

    Revert "fix(react-helmet): use react-helmet-async to fix console error"

--- src/index.jsx
+++ src/index.jsx
@@ -18,15 +18,12 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import { HelmetProvider } from "react-helmet-async";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
