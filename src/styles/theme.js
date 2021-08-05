2e6a49456e57420e4e23a016f58d5d0755c41adf
Shruti3004 <mail2shruti.ag@gmail.com>
2021-08-05 16:28:20 +0530

    fix(style): fixed the styling of header
    Signed-off-by: Shruti3004 <mail2shruti.ag@gmail.com>

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -24,6 +24,7 @@
 export const lightTheme = {
   primaryColor: "#DC3545",
   secondaryColor: "#A93232",
+  tertiaryColor: "#E5E5E5",
   body: "#FFFFFF",
   text: "#212121",
   primaryText: "#212121",
@@ -35,9 +36,10 @@ export const lightTheme = {
 export const darkTheme = {
   primaryColor: "#525252",
   secondaryColor: "#212121",
+  tertiaryColor: "#E5E5E5",
   body: "#212121",
   text: "#212121",
-  primaryText: "#FFF",
+  primaryText: "#FFFFFF",
   secondaryText: "#FFFFFF",
   toggleBorder: "#6B8096",
   hoverBackgroundColor: "#212121",
