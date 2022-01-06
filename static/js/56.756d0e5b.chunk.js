472439dcf2a4907f3cd17d5046f2e4a1575b10d0
GMishx <gmishx@gmail.com>
2022-01-06 04:49:07 +0000

    Deploying to gh-pages from @ fossology/FOSSologyUI@850d3c5153bc4fd02f4d87403da965debe5fee22 🚀

---
+++ static/js/56.756d0e5b.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpfossologyui=this.webpackJsonpfossologyui||[]).push([[56],{171:function(e,t,c){"use strict";c.r(t);var s=c(15),n=c(92),j=(c(0),c(3)),o=c(110),r=c(1),a=["component"];t.default=Object(j.i)((function(e){var t=e.component,c=Object(n.a)(e,a);return Object(r.jsx)(j.b,Object(s.a)(Object(s.a)({},c),{},{render:function(e){return Object(r.jsx)(r.Fragment,{children:Object(r.jsxs)("div",{className:"wrapper",children:[Object(r.jsx)(o.a,{}),Object(r.jsx)(t,Object(s.a)({},e))]})})}}))}))}}]);
+//# sourceMappingURL=56.756d0e5b.chunk.js.map
