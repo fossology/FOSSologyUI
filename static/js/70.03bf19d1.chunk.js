615bee7e2554a8673808e02150dd7dbb78fe40e9
shaheemazmalmmd <shaheem.azmal@siemens.com>
2022-09-06 14:34:15 +0000

    Deploying to gh-pages from @ fossology/FOSSologyUI@cf11daecde4950723052632fa6412a970933cdc0 🚀

--- static/js/70.03bf19d1.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpfossologyui=this.webpackJsonpfossologyui||[]).push([[70],{197:function(e,t,c){"use strict";c.r(t);var s=c(52),n=c(75),j=(c(0),c(3)),o=c(127),r=c(1),a=["component"];t.default=Object(j.j)((function(e){var t=e.component,c=Object(n.a)(e,a);return Object(r.jsx)(j.b,Object(s.a)(Object(s.a)({},c),{},{render:function(e){return Object(r.jsx)(r.Fragment,{children:Object(r.jsxs)("div",{className:"wrapper",children:[Object(r.jsx)(o.a,{}),Object(r.jsx)(t,Object(s.a)({},e))]})})}}))}))}}]);
-//# sourceMappingURL=70.03bf19d1.chunk.js.map
