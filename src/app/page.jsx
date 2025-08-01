f73b7e0d0595cb963f27dd483cfc496c7145e464
priyank patel <priyank.patel2341@gmail.com>
2025-08-01 22:32:30 +0530

    fix: 320 CSR without bailout updated <Suspense/> as per the guidelines

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,7 +8,7 @@ export const metadata = {
 
 export default function HomePage() {
   return (
-    <Suspense>
+    <Suspense fallback={<div>Loading home page...</div>}>
       <HomeClient />
     </Suspense>
   );
--- src/app/upload/reportImport/page.jsx
+++ src/app/upload/reportImport/page.jsx
@@ -7,7 +7,7 @@ export const metadata = {
 
 export default function ImportReportPage() {
   return (
-    <Suspense>
+    <Suspense fallback={<div>Loading report importer...</div>}>
       <ImportReportClient />
     </Suspense>
   );
